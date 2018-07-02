02058d1b975591fd61d9954a58d3a0c2548c1f1f
Sam Sepiol <38671028+e-corp-sam-sepiol@users.noreply.github.com>
2018-07-02 15:13:31 -0400

    Create install-vertnode.sh
    1.) Create vertnode install script, separate from the forked source. Preserved BTC donation address, thank you jochemin for your wonderful contribution to the greater community <3

---
+++ install-vertnode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# An automated script to assist with installing Vertcoin full node(s)
+# -------------------------------------------------------------------
+# AUTHORS:
+# jochemin   | BTC Donations --> 3FM6FypcrSVhdHh7cpVQMrhPXPZ6zcXeYU
+# Sam Sepiol |
+# -------------------------------------------------------------------
+
